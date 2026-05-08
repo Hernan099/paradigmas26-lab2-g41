1c29bac3541259d2805a0556a4686533c9d8e066
maeldesi <maeldesi@mi.unc.edu.ar>
2026-05-08 11:27:26 -0300

    Feat: implementada countByType ej 5

--- src/main/scala/Analyzer.scala
+++ src/main/scala/Analyzer.scala
@@ -60,6 +60,6 @@ object Analyzer {
    *                 )
    */
   def countByType(entities: List[NamedEntity]): Map[String, Int] = {
-    ???
+    entities.groupBy(_.entityType).mapValues(_.size)
   }
 }
