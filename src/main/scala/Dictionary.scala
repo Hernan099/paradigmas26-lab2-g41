c954b602e0f212b33b79d95a83603d1f77072a11
hernan <hpereyra599@gmail.com>
2026-05-08 11:23:53 -0300

    feat: completado dictionary ej 2

--- src/main/scala/Dictionary.scala
+++ src/main/scala/Dictionary.scala
@@ -18,6 +18,9 @@
  *   Python
  *   Haskell
  */
+import scala.io.Source
+//import NamedEntity._
+
 object Dictionary {
 
   /**
@@ -38,7 +41,24 @@ object Dictionary {
    *
    */
   def loadFromFile(filePath: String, entityType: String): List[NamedEntity] = {
-    ???
+    val source = Source.fromFile(filePath)
+
+    val lineas = try {
+      source.getLines().toList
+
+    } finally {
+      source.close()
+    }
+
+    entityType match {
+    case "person" => lineas.map(linea => new Person(linea))
+    case "organization" => lineas.map(linea => new Organization(linea))
+    case "university" => lineas.map(linea => new University(linea))
+    case "place" => lineas.map(linea => new Place(linea))
+    case "technology" => lineas.map(linea => new Technology(linea))
+    case "languages" => lineas.map(linea => new ProgrammingLanguage(linea))
+    case _ => List()
+    }
   }
 
   /**
@@ -47,9 +67,19 @@ object Dictionary {
    * @return lista con todas las entidades de todos los diccionarios
    *
    * TODO (Ejercicio 2): Implementar este método.
+   * languages organizations people places universities
    *
    */
   def loadAll(): List[NamedEntity] = {
-    ???
+    
+    val en1 = loadFromFile("data/places.txt","place")
+    val en2 = loadFromFile("data/languages.txt","languages")
+    val en3 = loadFromFile("data/people.txt","person")
+    val en4 = loadFromFile("data/universities.txt","university")
+    val en5 = loadFromFile("data/organizations.txt","organization")
+    val entidades = List.concat(en1, en2, en3, en4, en5)
+
+     entidades
+
   }
 }
